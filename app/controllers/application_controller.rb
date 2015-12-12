7ab956f38542861182551cb77bfe4d49a39f231c
RoRElessar <palantiri@ukr.net>
2015-12-12 21:35:52 +0200

    heroku fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   #before_filter :authenticate_user!, except: [:show, :index]
+
+  def current_cart
+    Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    cart
+  end
+
 end
