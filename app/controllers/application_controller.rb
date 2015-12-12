13f22177a35e1796071ce98c4e64c967c9b9f58c
RoRElessar <palantiri@ukr.net>
2015-12-12 20:50:15 +0200

    heroku fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,36 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   #before_filter :authenticate_user!, except: [:show, :index]
 
+ # def current_cart
+ #   Cart.find(session[:cart_id])
+ # rescue ActiveRecord::RecordNotFound
+ #   cart = Cart.create
+ #   session[:cart_id] = cart.id
+ #   cart
+ # end
+
   def current_cart
-    Cart.find(session[:cart_id])
-  rescue ActiveRecord::RecordNotFound
-    cart = Cart.create
-    session[:cart_id] = cart.id
-    cart
+
+  if session[:cart_id]
+    begin
+      @current_cart ||= Cart.find(session[:cart_id])
+      session[:cart_id] = nil if @current_cart.purchased_at
+    rescue ActiveRecord::RecordNotFound =>
+      create_cart
+    end
+  else
+    create_cart
   end
+
+    @current_cart
+  end
+
+  private
+
+  def create_cart
+    @current_cart = Cart.create!
+    session[:cart_id] = @current_cart.id
+  end
+
 
 end
