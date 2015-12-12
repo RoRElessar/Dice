b7723213034cc9feb44a45725a4aaffaf9f2d8d0
RoRElessar <palantiri@ukr.net>
2015-12-12 21:15:08 +0200

    heroku fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,37 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   #before_filter :authenticate_user!, except: [:show, :index]
-
- # def current_cart
- #   Cart.find(session[:cart_id])
- # rescue ActiveRecord::RecordNotFound
- #   cart = Cart.create
- #   session[:cart_id] = cart.id
- #   cart
- # end
-
-  def current_cart
-
-  if session[:cart_id]
-    begin
-      @current_cart ||= Cart.find(session[:cart_id])
-      session[:cart_id] = nil if @current_cart.purchased_at
-    rescue ActiveRecord::RecordNotFound =>
-      create_cart
-    end
-  else
-    create_cart
-  end
-
-    @current_cart
-  end
-
-  private
-
-  def create_cart
-    @current_cart = Cart.create!
-    session[:cart_id] = @current_cart.id
-  end
-
-
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,15 @@
 module ApplicationHelper
 
+  def current_cart
+    Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    cart
+  end
+
   def line_items_cart
-    @cart = Cart.find(session[:cart_id])
+    @cart = current_cart
     @line_items_cart = @cart.line_items
   end
 
