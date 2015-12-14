917584b6b500e9c4eaea11db9d00044adf7b107a
RoRElessar <palantiri@ukr.net>
2015-12-14 14:02:12 +0200

    added s3 feature

--- app/uploaders/product_image_uploader.rb
+++ app/uploaders/product_image_uploader.rb
@@ -7,7 +7,7 @@ class ProductImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  include CarrierWaveDirect::Uploader
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
