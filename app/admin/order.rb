727232a4e3c0f5d0a297f786d648ad9c32783f0e
RoRElessar <palantiri@ukr.net>
2015-12-27 18:29:22 +0200

    added total price, pay type & address to activeadmin orders

--- app/admin/order.rb
+++ app/admin/order.rb
@@ -14,22 +14,30 @@ ActiveAdmin.register Order do
 # end
 
   show do |order|
+    price = 0
     div do
-      panel('Items') do
+      panel('Товары') do
         table_for(order.line_items) do
           column 'Изображение' do |i|
-            image_tag i.product.product_image_url(:small) if i.product.product_image?
+            image_tag i.product.product_image_url(:cart) if i.product.product_image?
           end
           column 'Название' do |i|
             i.product.title
           end
           column 'Количество', :amount
           column 'Цена' do |i|
+            price = price + i.total_price
             number_to_currency(i.total_price)
           end
         end
       end
     end
+
+      div :class => 'panel' do
+        h3 "Общая стоимость: #{number_to_currency(price)}"
+        h3 "Способ оплаты: #{order.pay_type}"
+        h3 "Адресс доставки: #{order.address}"
+      end
   end
 
 end
