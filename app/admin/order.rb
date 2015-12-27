31070eb70e74a5cc959ae2aad6ed2d4698dea52e
RoRElessar <palantiri@ukr.net>
2015-12-27 18:36:41 +0200

    added permit_params to activeadmin orders

--- app/admin/order.rb
+++ app/admin/order.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Order do
+  permit_params :phone, :name, :email, :address, :pay_type
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
